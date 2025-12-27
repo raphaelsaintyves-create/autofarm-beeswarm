a1ff066cab39a60df3c403b9dea00f8eb8dc95b6
Raphael <raphaelsaintyves@gmail.com>
2025-12-27 23:12:01 +0100

    update to better farm

--- autoclick.py
+++ autoclick.py
@@ -5,7 +5,13 @@ import time
 import threading
 import keyboard
 
-touches = ['w', 'a', 's', 'd']
+touches = ['w', 'd', 's', 'd', 'w', 'd', 's', 'a']
+
+# Durées d'appui par touche (en secondes)
+# Séquence: w 2.5s, d 1s, s 2.5s, d 1s, w 2.5s, d 1s, s 2.5s, a 3s
+durations = {'w': 1.25, 'd': 0.5, 's': 1.25, 'a': 1.5}
+# Temps entre deux appuis (en secondes)
+gap_between_touches = 0
 
 # Event utilisé pour demander l'arrêt propre du programme
 stop_event = threading.Event()
@@ -34,14 +40,16 @@ try:
         for touche in touches:
             if stop_event.is_set():
                 break
+            # Récupère la durée d'appui pour la touche courante (par défaut 1s si manquant)
+            duration = durations.get(touche, 1.0)
             keyboard.press(touche)
-            time.sleep(2.5)
+            time.sleep(duration)
             keyboard.release(touche)
 
-            time.sleep(1)
+            time.sleep(gap_between_touches)
 except KeyboardInterrupt:
     stop_event.set()
 
-print("Stopped (Ctrl+J orq KeyboardInterrupt)")
+print("Stopped (Ctrl+J or KeyboardInterrupt)")
 
 
